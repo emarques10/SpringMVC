84783814b7b30f64986f4b62d9dbf2b40ebee5e4
emarques10 <marquescpv@gmail.com>
2019-05-02 09:24:51 -0300

    Configuração Inicial

---
+++ src/br/com/emarques/springmvc/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.emarques.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	//<servidor>/hello/message
+	
+	@RequestMapping("/message")
+	public String message() {
+		return "hello";
+	}
+
+}
