14089b9fb913cd6afc14db110b6509bbf678ac34
emarques10 <marquescpv@gmail.com>
2019-05-02 10:40:34 -0300

    Repassando informações das action para as views: A interface Model

--- src/br/com/emarques/springmvc/controllers/HelloController.java
+++ src/br/com/emarques/springmvc/controllers/HelloController.java
@@ -1,17 +1,26 @@
 package br.com.emarques.springmvc.controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 	
-	//<servidor>/hello/message
+	//<servidor>/app/hello/message
 	
 	@RequestMapping("/message")
 	public String message() {
 		return "hello";
+	}
+	
+	@RequestMapping("/mensagemDoServidor")
+	public String mensagemDoServidor(Model model) {
+		model.addAttribute("mensagem", new Date().toString());
+		return "mensagemDoServidor";
 	}
 
 }
